6ec866bac906b6ce253467b83ff0aaee694e74f9
titawidia <gitawidia166@gmail.com>
2023-02-08 08:39:54 +0700

    First Installing

---
+++ src/main/java/com/itc/ppdbsystem/PpdbSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itc.ppdbsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpdbSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpdbSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itc/ppdbsystem/PpdbSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.ppdbsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpdbSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
